e4323956d0c77eb126c7ae148b0548ac3bcec384
Joey Morelos <JoeyAM8402B@gmail.com>
2020-05-07 14:55:14 -0500

    changes to apiRoutes to allow testing in postman

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,21 +1,21 @@
 const router = require("express").Router();
 const db = require("../models");
 
-// Get the trivia data for the pokemon with the given id. 
-router.get("/api/pokemon/:id", (req, res) => {
+// Get the trivia data for all pokemon in the DB. 
+router.get("/api/pokemon", (req, res) => {
  
   db.Trivia.find({
-    title: { $regex: new RegExp(req.query.q, 'i')}
+
   })
     .then(trivia => res.json(trivia))
     .catch(err => res.status(422).end());
 });
 
-router.get("/api/size", (req, res) => {
-  db.Trivia.find({})
-  .then(data => {
-    res.json(data.length)
-  })
+router.get("/api/random/:count", (req, res) => {
+  db.Trivia.aggregate(
+    [{$sample: {size: req.params.count}}]
+  )
 })
+
 
 module.exports = router;

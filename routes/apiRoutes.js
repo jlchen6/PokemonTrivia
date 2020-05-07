87a50fd6b75f1b2f9e5907563ce6ab92ce27000d
Joey Morelos <JoeyAM8402B@gmail.com>
2020-05-07 14:11:22 -0500

    updates to API Router

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,21 @@
 const router = require("express").Router();
 const db = require("../models");
 
-router.get("/trivia", (req, res) => {
-  // Use a regular expression to search titles for req.query.q
-  // using case insensitive match. https://docs.mongodb.com/manual/reference/operator/query/regex/index.html
+// Get the trivia data for the pokemon with the given id. 
+router.get("/api/pokemon/:id", (req, res) => {
+ 
   db.Trivia.find({
     title: { $regex: new RegExp(req.query.q, 'i')}
   })
     .then(trivia => res.json(trivia))
     .catch(err => res.status(422).end());
 });
+
+router.get("/api/size", (req, res) => {
+  db.Trivia.find({})
+  .then(data => {
+    res.json(data.length)
+  })
+})
 
 module.exports = router;

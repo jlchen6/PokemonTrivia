a6fd4e2a4e1936bf28ceb9bc9a0892533605784e
Julia <jlchen6@wisc.edu>
2020-05-08 14:58:33 -0500

    finished up refactoring user API and userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,19 +12,29 @@ module.exports = {
         .then(dbUsers => res.json(dbUsers))
         .catch(err => res.json(err));
     },
-    createOrUpdate: function(user) { 
-        //If new user, create, else update user
-        return db.User
-            .find({username: user.username})
-            .then(dbUser => { //returns an array of user objects
-                if(dbUser.length > 0){
-                    //console.log(dbUser[0].username+" exists");
-                    return this.update(user);
-                } else {
-                    //console.log("User does not exist");
-                    return this.create(user);
-                }
-            }).catch(err => err);
-        //Catch is end of return db.User section
-    }
+    create: function(req, res) {
+        db.User.create(req.body)
+        .then(dbUser => res.json(dbUser))
+        .catch(err => res.json(err))
+    },
+    // update: function(req, res) {
+    //     db.User.update(req.body)
+    //     .then(dbUser => res.json(dbUser))
+    //     .catch(err => res.json(err))
+    // },
+    // createOrUpdate: function(user) { 
+    //     //If new user, create, else update user
+    //     return db.User
+    //         .find({username: user.username})
+    //         .then(dbUser => { //returns an array of user objects
+    //             if(dbUser.length > 0){
+    //                 //console.log(dbUser[0].username+" exists");
+    //                 return this.update(user);
+    //             } else {
+    //                 //console.log("User does not exist");
+    //                 return this.create(user);
+    //             }
+    //         }).catch(err => err);
+    //     //Catch is end of return db.User section
+    // }
 }

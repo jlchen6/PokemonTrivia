a48873721a557093fe7dc2321609f7fcf0352569
Julia <jlchen6@wisc.edu>
2020-05-09 16:53:58 -0500

    Added code to about.js to actually return react components

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,7 +4,7 @@ class About extends Component {
 
 
     render() {
-        <Container>
+        return (<Container>
             <h1>About the Game</h1>
             <p>
                 This is a pokemon trivia game meant to test your knowledge on pokemon! Up to 6 players can play at once, competing against the timer to correctly guess the pokemon.
@@ -14,7 +14,7 @@ class About extends Component {
                 To get started, log in on the main page. After logging in, you can start or enter a game session.
             </p>
 
-        </Container>
+        </Container>)
     }
 
 

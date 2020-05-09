00423f593f873baaa7336f757e5282a1c2227f42
fox-yokai <pat.w.cain@gmail.com>
2020-05-09 13:27:34 -0500

    merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from './logo.svg';
-import Button from './components/Button/button.js'
 import './App.css';
 
 function App() {
@@ -19,7 +18,6 @@ function App() {
         >
           Learn React
         </a>
-        <Button> Test Button </Button> 
       </header>
     </div>
   );

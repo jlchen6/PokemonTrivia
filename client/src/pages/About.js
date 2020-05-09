a00f0378323d1ac202942880a0fa1ceab8eb0d1e
Julia <jlchen6@wisc.edu>
2020-05-09 17:26:13 -0500

    adding some formatting to About.js

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -5,15 +5,20 @@ class About extends Component {
 
     render() {
         return (<Container>
-            <h1>About the Game</h1>
-            <p>
-                This is a pokemon trivia game meant to test your knowledge on pokemon! Up to 6 players can play at once, competing against the timer to correctly guess the pokemon.
-            </p>
-            <h1>How to Play</h1>
-            <p>
-                To get started, log in on the main page. After logging in, you can start or enter a game session.
-            </p>
-
+            <Row>
+                <Col>
+                    <h1>About the Game</h1>
+                    <p>
+                        This is a pokemon trivia game meant to test your knowledge on pokemon! Up to 6 players can play at once, competing against the timer to correctly guess the pokemon.
+                    </p>
+                </Col>
+                <Col>
+                    <h1>How to Play</h1>
+                    <p>
+                        To get started, log in on the main page. After logging in, you can start or enter a game session.
+                    </p>
+                </Col>
+            </Row>
         </Container>)
     }
 

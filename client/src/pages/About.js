cf428183788b912c34351ed8e4903bf4940b0104
Julia <jlchen6@wisc.edu>
2020-05-09 17:36:32 -0500

    Added bootstrap scripts to index.html and put columns into about.js

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -6,13 +6,13 @@ class About extends Component {
     render() {
         return (<Container>
             <Row>
-                <Col>
+                <Col size="md-5 sm-9">
                     <h1>About the Game</h1>
                     <p>
                         This is a pokemon trivia game meant to test your knowledge on pokemon! Up to 6 players can play at once, competing against the timer to correctly guess the pokemon.
                     </p>
                 </Col>
-                <Col>
+                <Col size="md-5 sm-9">
                     <h1>How to Play</h1>
                     <p>
                         To get started, log in on the main page. After logging in, you can start or enter a game session.

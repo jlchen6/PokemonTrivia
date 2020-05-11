5daaffb023ac1f77c1243de4f3a3ea0601aba3bd
Julia <jlchen6@wisc.edu>
2020-05-11 12:47:41 -0500

    added code to generate random pokemon on page load

--- client/src/pages/GameMode.js
+++ client/src/pages/GameMode.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
+import API from "../utils/API"
 
 class GameMode extends Component {
 
+    state = {
+        gameQuestions: [],
+        currQ: 0,
+        error: ""
+    }
+
+    componentDidMount() {
+        API.getRandom(5)
+            .then(res =>
+                this.setState({
+                    gameQuestions: res.data
+                })
+            )
+            .catch(err => this.setState({error: "Could not generate random pokemon"}))
+    }
 
     render() {
         return (
             <div>
-                
+
             </div>
         )
     }

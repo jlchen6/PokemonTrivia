8d4040186580da48e6dfd1b87e5c6925bbd619a9
Joey Morelos <JoeyAM8402B@gmail.com>
2020-05-11 11:20:53 -0500

    landing.js updates

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,23 +1,30 @@
 import React, { useEffect, useState } from "react";
-import Button from "../components/Button";
-import Thumbnail from "../components/Thumbnail/index.js";
-import Jumbotron from "../components/Jumbotron";
-import DeleteBtn from "../components/DeleteBtn";
+import Button from "../components/Button/button";
+import Thumbnail from "../components/Thumbnail/index";
+import Jumbotron from "../components/Jumbotron/index";
+import DeleteBtn from "../components/DeleteBtn/index";
 import API from "../utils/API";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 import { Input, TextArea, FormBtn } from "../components/Form";
 
-    return (
-      <div>
-     <h1>Name that Pokémon!</h1> 
+function Landing() {
+  return (
+    <div>
+      <img src="images/pokeball.gif" />
+      <h1>Name that Pokémon!</h1>
       <form>
-        <input>Username</input>
-        <input>Password</input>
+        <input id="userName" placeholder="Enter your Username">
+          Username
+        </input>
+        <input id="password" placeholder="Enter your password">
+          Password
+        </input>
         <button>Sign Up</button>
         <button>Login</button>
         <button>About</button>
       </form>
-      </div>
-    )
+    </div>
+  );
+}
 export default Landing;

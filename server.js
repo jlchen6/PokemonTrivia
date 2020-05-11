d2bde5db28a1b1673edb60d2c95f5964ba11d406
fox-yokai <pat.w.cain@gmail.com>
2020-05-10 20:50:57 -0500

    github redirect matches

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // allow to server to accept request from different origin
+    origin: "http://localhost:3001", // allow to server to accept request from different origin
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true // allow session cookie from browser to pass through
   })

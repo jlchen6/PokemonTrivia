dd060c1286acc6922a41da5b571e5e0a0532e5ff
fox-yokai <pat.w.cain@gmail.com>
2020-05-10 19:54:47 -0500

    changed port in the href link

--- server.js
+++ server.js
@@ -8,44 +8,9 @@ const routes = require("./routes");
 const app = express();
 
 const apiRoutes = require("./routes/api");
-const authRoutes = require("./routes/authRoutes");
+const authRoutes = require("./routes/authRoutes")
 
 const PORT = process.env.PORT || 3001;
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Configure body parsing for AJAX requests
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-// Define Github Strategy
-const GitHubStrategy = require("passport-github2").Strategy;
-
-let strategy = new GitHubStrategy(
-{
-  clientID: process.env.NODE_ENV === "production" ? process.env.GITHUB_CLIENT_ID_PRODUCTION : process.env.GITHUB_CLIENT_ID,
-  clientSecret: process.env.NODE_ENV === "production" ? process.env.GITHUB_CLIENT_SECRET_PRODUCTION : process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: process.env.NODE_ENV === "production" ? null : "/auth/github/callback"
-},
-  (accessToken, refreshToken, profile, done) => done(null, profile)
-);
-
-passport.use(strategy);
-
-
-// not configured
-let session = require("express-session")({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-});
-// Add routes, both API and view
-app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
@@ -56,6 +21,14 @@ mongoose.connect(
     useUnifiedTopology: true
   }
 );
+
+let session = require("express-session")({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+});
+
+app.use(session);
 
 passport.serializeUser((user, done) => done(null, user));
 
@@ -71,13 +44,49 @@ passport.deserializeUser((profile, done) => {
 
 });
 
-app.use(session);
-app.use(cors()); // allows the server to make requests to a different domain
+// Define Github Strategy
+const GitHubStrategy = require("passport-github2").Strategy;
+
+let strategy = new GitHubStrategy(
+{
+  clientID: process.env.NODE_ENV === "production" ? process.env.GITHUB_CLIENT_ID_PRODUCTION : process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.NODE_ENV === "production" ? process.env.GITHUB_CLIENT_SECRET_PRODUCTION : process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: process.env.NODE_ENV === "production" ? null : "/auth/github/callback"
+},
+  (accessToken, refreshToken, profile, done) => {
+    console.log("Authenticating with Github");
+    done(null, profile);
+  });
+
+  passport.use(strategy);
+
+// Add routes, both API and view
+app.use(routes);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Configure body parsing for AJAX requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use(
+  cors({
+    origin: "http://localhost:3000", // allow to server to accept request from different origin
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true // allow session cookie from browser to pass through
+  })
+);
 
 
 // Use apiRoutes
-app.use("/api", apiRoutes);
 app.use("/auth", authRoutes);
+app.use("/api", apiRoutes);
+
 
 // Start the API server
 app.listen(PORT, () =>

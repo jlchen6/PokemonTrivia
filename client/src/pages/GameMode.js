be9bee2362693a5dbb3287ee57ce5098b6343478
Julia <jlchen6@wisc.edu>
2020-05-11 13:13:45 -0500

    Added code to display first hint

--- client/src/pages/GameMode.js
+++ client/src/pages/GameMode.js
@@ -1,31 +1,55 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import API from "../utils/API"
+import { Question } from "../components/Question/question";
 
-class GameMode extends Component {
+function Landing() {
 
-    state = {
+    // state = {
+    //     gameQuestions: [],
+    //     currQNum: 0,
+    //     currQDex: "",
+    //     error: ""
+    // }
+    const [game, setGame] = useState({
         gameQuestions: [],
-        currQ: 0,
-        error: ""
-    }
+        currQ: 0
+    })
 
-    componentDidMount() {
+    const [trivia, setTrivia] = useState({
+        dexEntry: "",
+        pokemon: "",
+        hintImage: "",
+        spriteImage: "",
+        type: [],
+        choices: []
+    })
+
+    useEffect(() => {
         API.getRandom(5)
-            .then(res =>
-                this.setState({
+            .then(res => {
+                setGame({
                     gameQuestions: res.data
-                })
-            )
-            .catch(err => this.setState({error: "Could not generate random pokemon"}))
+                });
+                loadTrivia(game.gameQuestions[0]);
+                console.log(game.gameQuestions[0]);
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+    loadTrivia(question){
+        setTrivia(question);
+        const dex = question.dex[(Math.floor(Math.random()*question.dex.length))];
+        setTrivia({...trivia, dexEntry: dex});        
     }
 
-    render() {
-        return (
-            <div>
 
-            </div>
-        )
-    }
+    return (
+        <div>
+            <Question>
+                <p>Hint: {trivia.dexEntry}</p>
+            </Question>
+        </div>
+    )
 }
 
 export default GameMode;

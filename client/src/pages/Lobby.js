b0e917c3319500fd7015d61e9bf4c5416ad98179
Joey Morelos <JoeyAM8402B@gmail.com>
2020-05-13 12:22:08 -0500

    update to button in lobby to route to game mode

--- client/src/pages/Lobby.js
+++ client/src/pages/Lobby.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
 import API from "../utils/API";
 import Button from "../components/Button/button";
-import GithubAuth from "../components/GithubAuth/index";
 import Pokeball from "../images/pokeball.gif";
-
+import {Link} from 'react-router-dom'
 
 function Lobby() {
 
@@ -33,7 +32,9 @@ function Lobby() {
       <br/>
       <p>Players go here</p>
       <br/>
-      <button>Ready</button>
+      <Link to="/game">
+      <Button>Ready</Button>
+      </Link>
       <p>Timer Goes Here</p>
       </div>
          )}

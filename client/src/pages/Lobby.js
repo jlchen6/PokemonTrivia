edf7f0f002464db76334ce291a1d5a47b4314cf5
Joey Morelos <JoeyAM8402B@gmail.com>
2020-05-13 12:33:59 -0500

    link ready button in lobby to game

--- client/src/pages/Lobby.js
+++ client/src/pages/Lobby.js
@@ -2,12 +2,9 @@ import React, { useEffect, useState } from "react";
 import API from "../utils/API";
 import Button from "../components/Button/button";
 import Pokeball from "../images/pokeball.gif";
-<<<<<<< HEAD
 import {Link} from 'react-router-dom'
-=======
 import TrainerLobby from "../images/titles/TrainerLobby.png";
 
->>>>>>> f69bd6139be2201efedfe9c0ac5893b6dc8fbd88
 
 function Lobby() {
 
@@ -38,7 +35,7 @@ function Lobby() {
       <p>Players go here</p>
       <br/>
       <Link to="/game">
-      <Button>Ready</Button>
+      <button className="text-center justify-content-center">Ready</button>
       </Link>
       <p>Timer Goes Here</p>
       </div>

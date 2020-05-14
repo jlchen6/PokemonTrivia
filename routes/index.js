ecff714dbbf0ecba382f5b3ef008cf4d00c17584
fox-yokai <pat.w.cain@gmail.com>
2020-05-14 14:35:03 -0500

    reverting changes

--- routes/index.js
+++ routes/index.js
@@ -15,9 +15,9 @@ router.use("/api", apiRoutes);
 //     res.sendFile(path.join(__dirname, "/"))
 // );
 
-router.use("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
+// router.use("*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../client/build/index.html"));
+//   });
 
 
 module.exports = router;
